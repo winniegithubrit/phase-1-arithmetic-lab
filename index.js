1a2623c7066e2795057e9f432c096682d9583dc4
winniegithubrit <winnie.jomo@student.moringaschool.com>
2023-03-17 17:49:45 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply=num1 * num2;
+const random=Math.floor(Math.random()*10)
+let num3 = 19;
+let num4 = 5;
+let mod=num3%num4;
+let set = [2,6,12,20,17];
+let max = Math.max(...set);
+
+
+
+
+
+
